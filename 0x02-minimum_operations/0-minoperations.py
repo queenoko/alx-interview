aaf822e814a04b29c22564e888b4f3d688ce5278
queenoko <quimaudongwo@gmail.com>
2024-01-15 16:02:33 +0100

    mini operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""The function for Minimum Operations"""
+
+
+def primeFactorization(x):
+    """This will returns prime factorization elements of x"""
+    div = 2
+    array = list()
+    while (div <= x):
+        if x % div == 0:
+            array.append(div)
+            x /= div
+        else:
+            div += 1
+
+    return array
+
+def minOperations(n):
+    """This will calculates the fewest number of operations needed
+    to result in exactly n H characters in the file"""
+    min = 0
+    factors = [x for x in primeFactorization(n)]
+    occurences = {item: factors.count(item) for item in factors}
+    for k, v in occurences.items():
+        min += k * v
+    return min
